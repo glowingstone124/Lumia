2fb91dcd2a8419e141b55d02ae9ed4f2770b0e80
glowingstone124 <hanserofficial@outlook.com>
2024-09-29 15:54:12 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "2.3.4"
+}
+
+group = "ind.glowingstone.lumia"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-html-builder-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-cio-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.4")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.4")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
